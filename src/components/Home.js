722e6773e5fd52cb39347c70cf8cd36d1c263944
Numaan <manumaanowais809@gmail.com>
2023-10-20 20:47:51 +0530

    Removed Version Errors

--- src/components/Home.js
+++ src/components/Home.js
@@ -8,6 +8,20 @@ function Home() {
     <div className='home-page'>
       <Header />
       <div className='main-home-content'>
+        <section className="featured-products">
+          <h2>Featured Products</h2>
+        </section>
+        <section className="trending">
+          <h2>Trending Now</h2>
+        </section>
+        <section className="newsletter">
+          <h2>Subscribe to our Newsletter</h2>
+          <p>Stay updated with the latest trends and offers.</p>
+          <form>
+            <input type="email" placeholder="Enter your email" />
+            <button type="submit">Subscribe</button>
+          </form>
+        </section>
       </div>
       <Footer />
     </div>
